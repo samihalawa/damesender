d2eb1e979e1e9692ec506a46217177674dbc41f7
leoburgos07 <leonardobh96@gmail.com>
2021-01-14 23:05:45 -0500

    Agregando el tipo administrador al registro

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'type' => 'Cliente',
+            'type' => 'Administrador',
             'status' => 1,
             'phone' => $data['phone'],
             
