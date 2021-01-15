3cfc3770560e3eac793f3c50a92c0b301dd73d81
leoburgos07 <leonardobh96@gmail.com>
2021-01-14 23:09:36 -0500

    REvirtiendo el cambio de tipo a cliente

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -69,7 +69,7 @@ class RegisterController extends Controller
             'name' => $data['name'],
             'email' => $data['email'],
             'password' => Hash::make($data['password']),
-            'type' => 'Administrador',
+            'type' => 'Cliente',
             'status' => 1,
             'phone' => $data['phone'],
             
