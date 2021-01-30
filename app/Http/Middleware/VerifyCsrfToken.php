8a290fa56a0d88303b3bb36bb470bccb53f71923
houltman <houltman_gonzalez@hotmail.com>
2021-01-30 01:48:25 -0400

    fix:excepcion url aws

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array
      */
     protected $except = [
-        '/amazon-sns/email-notifications'
+        '/amazon/webhook-email-notifications'
     ];
 }
