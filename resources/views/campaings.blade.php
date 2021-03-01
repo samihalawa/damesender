0f016f0cc4a8e7a4caf4db62f60b7f5c09114547
houltman <houltman_gonzalez@hotmail.com>
2021-02-28 23:30:48 -0400

    fix: tracking aws notification

--- resources/views/campaings.blade.php
+++ resources/views/campaings.blade.php
@@ -20,7 +20,7 @@
             <tr>
                 <th style="color:black" >{{ $campaing->name }}</th>
                 <th style="color:black" >{{ $campaing->sends->count() }}</th>
-                <th style="color:black" >{{ $campaing->sends->where('delivered ', 1)->count() }}</th>
+                <th style="color:black" >{{ $campaing->sends->where('delivered', 1)->count() }}</th>
                 <th style="color:black" >{{ $campaing->sends->where('opened', 1)->count() }}</th>
                 <th style="color:black" >{{ $campaing->sends->where('bounced ', 1)->count() }}</th>
                  
