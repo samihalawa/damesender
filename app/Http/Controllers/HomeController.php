a2a5e72671807b4715a7d6b0bc5133b214b7d7e1
Gabriel Houltman <houltman_gonzalez@hotmail.com>
2022-05-31 00:46:54 -0400

    emails para ventas de dominios

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use Exception;
 
 class HomeController extends Controller
 {
@@ -24,5 +26,36 @@ class HomeController extends Controller
     public function index()
     {
         return view('index');
+    }
+
+    public function sendEmail(Request $request){
+
+      $email='chenggong2649@gmail.com';
+      $email='houltman@gmail.com';
+      try{
+        $info = (object) [
+          'to_email_address' => $email,
+          'subject' => "My offer for Domain ".$request->domain,
+          'infofrom' => 'info@damesender.com',
+          'infoname' => $request->name,
+          'body' => "My offer for Domain",
+
+      ];
+
+
+        Mail::send("emails.domain", ['name' => $request->name,'email' => $request->email,'domain' => $request->domain,'phone' => $request->phone,'price' => $request->price,'comments'=>$request->comments] , function ($message) use (&$headers, $info) {
+          $message->to($info->to_email_address)
+              ->from($info->infofrom, $info->infoname)
+              ->subject($info->subject);
+          $headers = $message->getHeaders();
+      });
+      return ["success"=>'ok'];
+
+    }catch(Exception $e){
+      return ["success"=>'error'];
+    }
+
+
+
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,20 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
-|
+|URL: http://damesender.local/api/
+
 | Here is where you can register API routes for your application. These
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
-
+Route::post('/domain-purchase', [HomeController::class, 'sendEmail'])->name('sendEmail');
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
